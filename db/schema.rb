8b02b7bba95f76dd74729f66fd11626476e26431
keitarou516 <keitarou516@icloud.com>
2021-01-22 02:21:35 +0900

    created excesses tabel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_165532) do
+ActiveRecord::Schema.define(version: 2021_01_21_170049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,19 @@ ActiveRecord::Schema.define(version: 2021_01_21_165532) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "excesses", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.text "mail"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "resource"
+    t.text "text"
+    t.integer "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
