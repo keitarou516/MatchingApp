03c56006d7637be1feab6d0def30283217de2733
keitarou516 <keitarou516@icloud.com>
2021-01-22 02:19:56 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root "companies#show"
-
   resources :companies, only: [:show, :edit, :update, :destroy] do
+    resources :sites do
+      collection do
+        get 'search'
+      end
+    end
+    resources :excesses do
+      collection do
+        get 'search'
+      end
+    end
   end
 
 end
