1f14ca18e01cd920428683002e9c13c003ef8eeb
keitarou516 <keitarou516@icloud.com>
2021-01-22 23:26:41 +0900

    Implementation of company and costruction site registration function

--- app/controllers/excesses_controller.rb
+++ app/controllers/excesses_controller.rb
@@ -5,4 +5,13 @@ class ExcessesController < ApplicationController
 		@excess = Excess.new
 	end
 
+	def create
+		Excess.create(excess_params)
+	end
+
+	private
+
+	def excess_params
+		params.require(:excess).permit(:name, :start_date, :end_date, :resource, :phone, :mail, :text).merge(company_id: params[:company_id])
+	end
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -5,4 +5,14 @@ class SitesController < ApplicationController
 		@site = Site.new
 	end
 
+	def create
+		Site.create(site_params)
+	end
+
+	private
+
+	def site_params
+		params.require(:site).permit(:name, :address, :start_date, :end_date, :resource, :wage, :phone, :mail, :text).merge(company_id: params[:company_id])
+	end
+
 end
