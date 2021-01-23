274fb5daece9cdbe2b81b1fdcb01b6c627a6ad19
keitarou516 <keitarou516@icloud.com>
2021-01-23 22:47:32 +0900

    created destroy function of excess infomation

--- app/controllers/excesses_controller.rb
+++ app/controllers/excesses_controller.rb
@@ -1,22 +1,33 @@
 class ExcessesController < ApplicationController
 
+	before_action :set_company, except: [:create, :destroy]
+
 	def index
-		@company = Company.find(current_user.company_id)
 		@excesses = Excess.where(company_id: params[:company_id])
 	end
 
 	def new
-		@company = Company.find(current_user.company_id)
 		@excess = Excess.new
 	end
 
 	def create
 		Excess.create(excess_params)
+		redirect_to action: :index
+	end
+
+	def destroy
+		excess = Excess.find(params[:id])
+		excess.destroy
+		redirect_to action: :index
 	end
 
 	private
 
 	def excess_params
 		params.require(:excess).permit(:name, :start_date, :end_date, :resource, :phone, :mail, :text).merge(company_id: params[:company_id])
+	end
+
+	def set_company
+		@company = Company.find(current_user.company_id)
 	end
 end
