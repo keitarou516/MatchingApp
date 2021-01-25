b2693a59bb95b0ae5a3b32724307fa0c0c0928de
keitarou516 <keitarou516@icloud.com>
2021-01-25 17:31:15 +0900

    modified update action and refuctored sites_controller.rb

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,9 +1,10 @@
 class SitesController < ApplicationController
 
 	before_action :set_company, except: [:create]
+	before_action :set_site, only: [:edit, :update, :destroy]
 	
 	def index
-		@sites = Site.where(company_id: params[:company_id])
+		@sites = Site.where(company_id: params[:company_id]).order("created_at DESC")
 	end
 
 	def new
@@ -12,28 +13,30 @@ class SitesController < ApplicationController
 
 	def create
 		Site.create(site_params)
-		redirect_to action: :index
+		go_index
 	end
 
 	def edit
-		@site = Site.find(params[:id])
 	end
 
 	def	update
-		Site.update(site_params)
+		@site.update(site_params)
 		go_index
 	end
 
 	def destroy
-		site = Site.find(params[:id])
-		site.destroy
-		redirect_to action: :index
+		@site.destroy
+		go_index
 	end
 
 	private
 
 	def site_params
 		params.require(:site).permit(:name, :address, :start_date, :end_date, :resource, :wage, :phone, :mail, :text).merge(company_id: params[:company_id])
+	end
+
+	def set_excess
+		@site = Site.find(params[:id])
 	end
 
 end
