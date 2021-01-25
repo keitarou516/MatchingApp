88af954868fd3328233104c6b735ff828a2f0220
keitarou516 <keitarou516@icloud.com>
2021-01-25 17:28:51 +0900

    modified update action and refuctored excesses_controller.rb

--- app/controllers/excesses_controller.rb
+++ app/controllers/excesses_controller.rb
@@ -1,9 +1,10 @@
 class ExcessesController < ApplicationController
 
 	before_action :set_company, except: [:create, :destroy]
+	before_action :set_excess, only: [:edit, :update, :destroy]
 
 	def index
-		@excesses = Excess.where(company_id: params[:company_id])
+		@excesses = Excess.where(company_id: params[:company_id]).order("created_at DESC")
 	end
 
 	def new
@@ -16,17 +17,15 @@ class ExcessesController < ApplicationController
 	end
 
 	def edit
-		@excess = Excess.find(params[:id])
 	end
 
 	def	update
-		Excess.update(excess_params)
+		@excess.update(excess_params)
 		go_index
 	end
 
 	def destroy
-		excess = Excess.find(params[:id])
-		excess.destroy
+		@excess.destroy
 		go_index
 	end
 
@@ -34,6 +33,10 @@ class ExcessesController < ApplicationController
 
 	def excess_params
 		params.require(:excess).permit(:name, :start_date, :end_date, :resource, :phone, :mail, :text).merge(company_id: params[:company_id])
+	end
+
+	def set_excess
+		@excess = Excess.find(params[:id])
 	end
 
 end
