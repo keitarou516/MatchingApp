c4e9b159e537300c933d666606fb8e31bda0dc02
keitarou516 <keitarou516@icloud.com>
2021-01-27 23:21:06 +0900

    created User editing function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,24 @@ class UsersController < ApplicationController
 	def show
 		@company = Company.find(current_user.company_id)		
 	end
+
+	def edit
+		@company = Company.find(current_user.company_id)		
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		current_user.update(name: user_params[:name], email: user_params[:email], company_key: user_params[:company_key])
+		company = Company.find(current_user.company_id)
+		company.update(url: user_params[:url])
+		redirect_to action: :show		
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :company_key, :url)
+	end
+
 end
