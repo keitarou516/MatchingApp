c75757bfc9286df16cd1fee4a13a64a75456505a
keitarou516 <keitarou516@icloud.com>
2021-02-01 21:02:59 +0900

    added strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       devise_parameter_sanitizer.permit(:sign_in, keys:[:company_key])
       devise_parameter_sanitizer.permit(:sign_up, keys:[:company_key])
       devise_parameter_sanitizer.permit(:account_update, keys:[:company_key])
